1591dc8476dacf815d7183a60731e499e6e2eda1
Calvin Dito Pratama <calvindito7@gmail.com>
2023-01-10 22:43:00 +0700

    layouting

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>RSUD Pare - {{ $title }}</title>
+	<link href="{{ asset('template/assets/fonts/inter/inter.css') }}" rel="stylesheet">
+	<link href="{{ asset('template/assets/icons/phosphor/styles.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('template/assets/css/ltr/all.min.css') }}" id="stylesheet" rel="stylesheet">
+	<script src="{{ asset('template/assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('template/assets/js/app.js') }}"></script>
+</head>
