035a4409d40e5ac597e30f4b547e50637868f254
Calvin Dito Pratama <calvindito7@gmail.com>
2023-01-10 19:48:32 +0700

    set env & database connection

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+class DashboardController extends Controller {
 
-class DashboardController extends Controller
-{
-    //
+    public function index()
+    {
+        $data = [
+            'title' => 'Dashboard',
+            'content' => 'dashboard'
+        ];
+
+        return view('layouts.index', ['data' => $data]);
+    }
+
 }
