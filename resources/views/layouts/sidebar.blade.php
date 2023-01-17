3afbe074e3a53d7751df0435e12fb03ce4856f70
Calvin Dito Pratama <calvindito7@gmail.com>
2023-01-17 20:21:15 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -27,14 +27,63 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="nav-item nav-item-submenu">
+                    <li class="nav-item nav-item-submenu {{ Request::segment(1) == 'master-data' ? 'nav-item-expanded nav-item-open' : '' }}">
                         <a href="#" class="nav-link">
                             <i class="ph-archive"></i>
                             <span>Master Data</span>
                         </a>
-                        <ul class="nav-group-sub collapse">
-                            <li class="nav-item">
-                                <a href="javascript:void(0)" class="nav-link">Dropdown 1</a>
+                        <ul class="nav-group-sub collapse {{ Request::segment(1) == 'master-data' ? 'show' : '' }}">
+                            <li class="nav-item nav-item-submenu {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' ? 'nav-item-expanded nav-item-open' : '' }}">
+                                <a href="#" class="nav-link">General</a>
+                                <ul class="nav-group-sub collapse {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' ? 'show' : '' }}">
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/class-type') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'class-type' ? 'active' : '' }}">Kelas</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/doctor') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'doctor' ? 'active' : '' }}">Dokter</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/dtd') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'dtd' ? 'active' : '' }}">DTD</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/employee') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'employee' ? 'active' : '' }}">Karyawan</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/medical-service') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'medical-service' ? 'active' : '' }}">Pelayanan Medik</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/medicine') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'medicine' ? 'active' : '' }}">Obat</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/patient') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'patient' ? 'active' : '' }}">Pasien</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/patient-group') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'patient-group' ? 'active' : '' }}">Golongan Pasien</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/religion') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'religion' ? 'active' : '' }}">Agama</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/general/unit') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'general' && Request::segment(3) == 'unit' ? 'active' : '' }}">Unit</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item nav-item-submenu {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'location' ? 'nav-item-expanded nav-item-open' : '' }}">
+                                <a href="#" class="nav-link">Wilayah</a>
+                                <ul class="nav-group-sub collapse {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'location' ? 'show' : '' }}">
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/location/province') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'location' && Request::segment(3) == 'province' ? 'active' : '' }}">Provinsi</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/location/city') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'location' && Request::segment(3) == 'city' ? 'active' : '' }}">Kota</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/location/district') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'location' && Request::segment(3) == 'district' ? 'active' : '' }}">Kecamatan</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ url('master-data/location/village') }}" class="nav-link {{ Request::segment(1) == 'master-data' && Request::segment(2) == 'location' && Request::segment(3) == 'village' ? 'active' : '' }}">Kelurahan</a>
+                                    </li>
+                                </ul>
                             </li>
                         </ul>
                     </li>
